07016750ec9e2a1c8661727785a81faa30d09156
Henrique Nocera <henrique@axioscorp.com.br>
2020-11-18 16:55:36 -0300

    base

---
+++ ConsultaCEP-master/ConsultaCEP-master/README.md
@@ -0,0 +1,3 @@
+﻿# ConsultaCEP
+ 
+ Projeto para realizar buscas de Códigos de Endereçamento Postal (CEP) do Brasil.
---
+++ app.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado')
+const cep = document.querySelector('.cep');
+const estado = document.querySelector('.estado');
+const cidade = document.querySelector('.cidade');
+const bairro = document.querySelector('.bairro');
+const endereco = document.querySelector('.endereco');
+form.addEventListener('submit', consultaCEP);
+
+
+
+function consultaCEP(e) {
+    let inputCEP = document.querySelector('.input').value;
+    const url = `https://viacep.com.br/ws/${inputCEP}/json/`;
+
+    if (inputCEP.length < 8) {
+        window.alert('Por gentileza, digite um CEP válido')
+    } else {
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if (data.erro) {
+                window.alert('Por gentileza, digite um CEP válido')
+            } else {
+                cep.innerHTML = data.cep;
+                estado.innerHTML = data.uf;
+                cidade.innerHTML = data.localidade;
+                bairro.innerHTML = data.bairro;
+                endereco.innerHTML = data.logradouro;
+                form.reset();
+            }
+            console.log(data)
+        });
+    }
+    e.preventDefault();
+}
