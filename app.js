3d92958864072bdcf2f24c82a17a50796e17ba3c
hacom <66780190+hacom@users.noreply.github.com>
2020-11-18 20:35:13 -0300

    Updates

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ const endereco = document.querySelector('.endereco');
 form.addEventListener('submit', consultaCEP);
 
 
-
 function consultaCEP(e) {
     let inputCEP = document.querySelector('.input').value;
     const url = `https://viacep.com.br/ws/${inputCEP}/json/`;
 
     if (inputCEP.length < 8) {
         window.alert('Por gentileza, digite um CEP válido')
-    } else {
+    }  else  {
         fetch(url)
         .then(response => {
             return response.json();
@@ -24,15 +23,29 @@ function consultaCEP(e) {
             if (data.erro) {
                 window.alert('Por gentileza, digite um CEP válido')
             } else {
-                cep.innerHTML = data.cep;
-                estado.innerHTML = data.uf;
-                cidade.innerHTML = data.localidade;
-                bairro.innerHTML = data.bairro;
-                endereco.innerHTML = data.logradouro;
-                form.reset();
+                hideCEP();
+                setTimeout(function() {
+                    cep.innerHTML = data.cep;
+                    estado.innerHTML = data.uf;
+                    cidade.innerHTML = data.localidade;
+                    bairro.innerHTML = data.bairro;
+                    endereco.innerHTML = data.logradouro;
+                    form.reset();
+                    showCEP();
+                }, 800);
             }
             console.log(data)
         });
     }
     e.preventDefault();
 }
+
+function showCEP() {
+    resultado.style.opacity = '1';
+    resultado.style.transform = 'translateY(0px)';
+}
+function hideCEP() {
+    resultado.style.opacity = '0'
+    resultado.style.transform = 'translateY(100px)';
+}
+
